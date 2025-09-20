d6e5f234a9045917c432c8d6d75684702104b7db
scrubware <tomomnom99@gmail.com>
2025-09-19 19:31:55 -0600

    move/piece/position done

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -7,6 +10,8 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessBoard {
+
+    final private ChessPiece[][] board = new ChessPiece[8][8];
 
     public ChessBoard() {
         
@@ -19,8 +24,20 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        board[position.getRow()-1][position.getColumn()-1] = piece;
     }
+
+    /*private void addPiece(int row, int col, ChessGame.TeamColor team, ChessPiece.PieceType type) {
+        board[row-1][col-1] = new ChessPiece(team,type);
+    }*/
+
+    public void removePiece(ChessPosition position) {
+        board[position.getRow()-1][position.getColumn()-1] = null;
+    }
+
+    /*private void removePiece(int row, int col) {
+        board[row-1][col-1] = null;
+    }*/
 
     /**
      * Gets a chess piece on the chessboard
@@ -30,14 +47,64 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        return board[position.getRow()-1][position.getColumn()-1];
     }
+
+    /*private ChessPiece getPiece(int row, int col) {
+        return board[row-1][col-1];
+    }*/
+
+    /*private void createPawnRow(int row, ChessGame.TeamColor team) {
+        for (int col = 1; col <= 8; col ++) {
+            var position = new ChessPosition(row,col);
+            var piece = new ChessPiece(team, ChessPiece.PieceType.PAWN);
+            addPiece(position,piece);
+        }
+    }*/
+
+    /*private void createBackRow(int row, ChessGame.TeamColor team) {
+        addPiece(row,1,team, ChessPiece.PieceType.ROOK);
+        addPiece(row,2,team, ChessPiece.PieceType.KNIGHT);
+        addPiece(row,3,team, ChessPiece.PieceType.BISHOP);
+        addPiece(row,4,team, ChessPiece.PieceType.QUEEN);
+        addPiece(row,5,team, ChessPiece.PieceType.KING);
+        addPiece(row,6,team, ChessPiece.PieceType.BISHOP);
+        addPiece(row,7,team, ChessPiece.PieceType.KNIGHT);
+        addPiece(row,8,team, ChessPiece.PieceType.ROOK);
+    }*/
+
+    /*public void createStartingBoard() {
+        createBackRow(1,ChessGame.TeamColor.WHITE);
+        createPawnRow(2,ChessGame.TeamColor.WHITE);
+
+        createBackRow(8,ChessGame.TeamColor.BLACK);
+        createPawnRow(7,ChessGame.TeamColor.BLACK);
+    }*/
 
     /**
      * Sets the board to the default starting board
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        for (int row = 0; row < 8; row ++) {
+            for (int col = 0; col < 8; col ++) {
+                board[row][col] = null;
+            }
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        ChessBoard that = (ChessBoard) o;
+        return Arrays.deepEquals(board, that.board);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.deepHashCode(board);
     }
 }
--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,6 +1,8 @@
 package chess;
 
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Objects;
 
 /**
  * Represents a single chess piece
@@ -10,7 +12,26 @@ import java.util.Collection;
  */
 public class ChessPiece {
 
-    public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+    ChessGame.TeamColor color;
+    ChessPiece.PieceType type;
+
+    public ChessPiece(ChessGame.TeamColor color, ChessPiece.PieceType type) {
+        this.color = color;
+        this.type = type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessPiece that = (ChessPiece) o;
+        return color == that.color && type == that.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(color, type);
     }
 
     /**
@@ -29,14 +50,14 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return color;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return type;
     }
 
     /**
@@ -47,6 +68,6 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+        return new HashSet<ChessMove>();
     }
 }

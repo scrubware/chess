efe16ca4081d4180e17e08cd3140a9eaf3e0fe97
scrubware <tomomnom99@gmail.com>
2025-10-20 21:08:32 -0600

    server structure

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -2,6 +2,10 @@ package server;
 
 import io.javalin.*;
 
+import handlers.AdminHandler;
+import handlers.UserHandler;
+import handlers.GameHandler;
+
 public class Server {
 
     private final Javalin javalin;
@@ -10,6 +14,19 @@ public class Server {
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
 
         // Register your endpoints and exception handlers here.
+
+        var adminHandler = new AdminHandler();
+        javalin.delete("/db",adminHandler::handleClear);
+
+        var userHandler = new UserHandler();
+        javalin.post("/user",userHandler::handleRegister);
+        javalin.post("/session",userHandler::handleLogin);
+        javalin.delete("/session",userHandler::handleLogout);
+
+        var gameHandler = new GameHandler();
+        javalin.get("/game",gameHandler::handleListGames);
+        javalin.post("/game",gameHandler::handleCreateGame);
+        javalin.put("/game",gameHandler::handleJoinGame);
 
     }
 

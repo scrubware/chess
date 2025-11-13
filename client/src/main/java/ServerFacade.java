bbfc274de171d52b6c6bf57ab66a0a52e2a9421e
scrubware <tomomnom99@gmail.com>
2025-11-12 23:58:36 -0700

    serverfacade skeleton

---
+++ client/src/main/java/ServerFacade.java
@@ -0,0 +1,20 @@
+import model.AuthData;
+
+public class ServerFacade {
+
+    String help() {
+        return null;
+    }
+
+    void quit() {
+
+    }
+
+    AuthData register(String username, String password, String email) {
+        return null;
+    }
+
+    AuthData login(String username, String password) {
+        return null;
+    }
+}
